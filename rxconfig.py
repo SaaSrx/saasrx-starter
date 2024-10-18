1b8ce2915deb8c030d99278c26f6f922b0e78a4c
graham <graham.annett@gmail.com>
2024-10-18 15:04:36 -0600

    commit @ 2410181504

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,5 @@
-import rxext.lib as rxe
+from saas.rxext.config import Config
 
-config = rxe.Config(
-    module_path="src/saasrx/app",
-    app_name="saasrx",
-    theme=rxe.ThemeConfig(),
+config = Config(
+    app_name="saas",
 )

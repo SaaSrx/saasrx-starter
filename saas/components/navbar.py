2c7a5d64fe9de9728b685ee6fc4c9cacd0d2e8c2
graham <graham.annett@gmail.com>
2024-11-03 16:16:21 -0700

    Refactor navbar component; enhance menu item handling and streamline rendering logic

--- saas/components/navbar.py
+++ saas/components/navbar.py
@@ -1,19 +1,14 @@
 import reflex as rx
 
-import rxconfig
-from saas.state import MenuState
+from saas.state import MenuItem, MenuState
 
-# Define your MenuLinks here
-MenuLinks: dict[str, str] = {
-    # href: link-text
-}
 
 # ---- Navbar General Components
 
 
-def navbar_icon() -> rx.Component:
+def navbar_icon(app_icon: str = "rocket") -> rx.Component:
     return rx.box(
-        rx.box(rx.icon("rocket", class_name="h-8 w-8"), class_name="text-indigo-600"),
+        rx.box(rx.icon(app_icon, class_name="h-8 w-8"), class_name="text-indigo-600"),
         rx.text(
             MenuState.menu_title,
             class_name="ml-2 text-xl font-bold text-gray-900",
@@ -23,34 +18,90 @@ def navbar_icon() -> rx.Component:
     )
 
 
+def make_menu_items(items: list[MenuItem], link_func: callable, btn_func: callable):
+    def inner_func(item):
+        return rx.match(
+            item.typeof,
+            ("link", link_func(item.text, href=item.link)),
+            ("button", btn_func(item.text, href=item.link)),
+            rx.text("Unknown MenuType."),
+        )
+
+    # rather than have inner_fn and outer, just apply foreach here
+    return rx.foreach(items, inner_func)
+
+
 # ---- Navbar Desktop Components
 
 
 def navbar_desktop() -> rx.Component:
     link_style = "text-gray-600 hover:text-gray-900"
-    btn_style = "bg-indigo-600 text-white px-6 py-2 rounded-lg hover:bg-indigo-700 transition-colors"
+    btn_style = "text-white px-6 py-2 rounded-lg bg-indigo-500 hover:bg-indigo-700 transition-colors"
+
+    def link_func(text, href):  # or `partial(rx.link, class_name=link_style)`
+        return rx.link(text, href=href, class_name=link_style)
+
+    def btn_func(text, href):
+        return rx.link(rx.button(text, class_name=btn_style), href=href)
+
     return rx.box(
-        rx.link("Features", href="#features", class_name=link_style),
-        rx.link("Pricing", href="#pricing", class_name=link_style),
-        rx.link("Testimonials", href="#testimonials", class_name=link_style),
-        rx.link(rx.button("Get Started", class_name=btn_style), href="/signin"),
+        make_menu_items(items=MenuState.menu_items, link_func=link_func, btn_func=btn_func),
         class_name="hidden md:flex items-center space-x-8",
     )
 
 
-def navbar_desktop_open() -> rx.Component:
-    return rx.box()
+def navbar_desktop_() -> rx.Component:
+    link_style = "text-gray-600 hover:text-gray-900"
+    btn_style = "bg-indigo-600 text-white px-6 py-2 rounded-lg hover:bg-indigo-700 transition-colors"
+
+    def handler(item):
+        return rx.match(
+            item.typeof,
+            ("link", rx.link(item.text, href=item.link, class_name=link_style)),
+            ("button", rx.link(rx.button(item.text, class_name=btn_style), href=item.link)),
+            rx.text("Unknown MenuType."),
+        )
+
+    return rx.box(
+        rx.foreach(MenuState.menu_items, handler),
+        class_name="hidden md:flex items-center space-x-8",
+    )
 
 
 # ---- Navbar Mobile Components
 
 
 def navbar_mobile() -> rx.Component:
-    return rx.box()
+    # link_style = "text-gray-600 hover:text-gray-900"
+    link_style = ""
+    btn_style = "px-6 py-2 rounded-lg bg-indigo-500 hover:bg-indigo-800 text-white transition-colors"
+    # btn_style = "bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700"
+    # btn_style = ""
 
+    # def handler(item):
+    #     return rx.match(
+    #         item.typeof,
+    #         ("link", rx.menu.item(item.text, href=item.link, class_name=link_style)),
+    #         ("button", rx.menu.item(item.text, href=item.link, class_name=btn_style)),
+    #         rx.text("Unknown MenuType."),
+    #     )
 
-def navbar_menu_open() -> rx.Component:
-    return rx.box()
+    def link_func(text, href):  # or `partial(rx.link, class_name=link_style)`
+        return rx.menu.item(text, href=href, class_name=link_style)
+
+    def btn_func(text, href):
+        return rx.menu.item(text, href=href, class_name=btn_style)
+
+    return rx.box(
+        rx.menu.root(
+            rx.menu.trigger(
+                rx.icon("menu", size=30),
+            ),
+            rx.menu.content(,
+                make_menu_items(items=MenuState.menu_items, link_func=link_func, btn_func=btn_func),
+            ),
+        )
+    )
 
 
 def navbar_items() -> rx.Component:
@@ -58,10 +109,9 @@ def navbar_items() -> rx.Component:
         rx.box(
             navbar_icon(),
             # Desktop navigation links
-            # rx.desktop_only(navbar_desktop()),
-            navbar_desktop(),  # just do desktop until mobile is ready
+            rx.desktop_only(navbar_desktop()),
             # Mobile menu button
-            # rx.mobile_and_tablet(navbar_mobile()),
+            rx.mobile_and_tablet(navbar_mobile()),
             class_name="flex justify-between items-center h-16",
         ),
         class_name="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8",
@@ -74,10 +124,7 @@ def navbar():
         # Main container with styling
         navbar_items(),
         # Mobile navigation menu
-        rx.cond(
-            MenuState.is_open,
-            navbar_menu_open(),
-        ),
         # Styling for the nav component
-        class_name="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur-md z-50 border-b border-gray-100",
+        # class_name="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur-md z-50 border-b border-gray-100",
+        class_name="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur-md border-b border-gray-100",
     )
